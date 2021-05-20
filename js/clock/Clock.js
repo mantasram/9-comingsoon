a3892dd09e4eef8e0544387b8cb372f64ab2efc4
mramo21 <mantasramo@gmail.com>
2021-05-20 20:44:24 +0300

    Clock as a class

--- js/clock/Clock.js
+++ js/clock/Clock.js
@@ -1,4 +1,67 @@
-function Clock(selector) {
+class Clock {
+    constructor(selector) {
+        this.selector = selector;
+        this.DOM = null;
+
+        this.init();
+
+    }
+
+    init() {
+        if (!this.isValidSelector()) {
+            return false;
+        }
+
+        this.render();
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            console.error('ERROR: selector has to be a non-empty string');
+            return false
+        }
+
+        this.DOM = document.querySelector(this.selector);
+        if (!this.DOM) {
+            console.error('ERROR: could not find an element with given selector')
+            return false;
+        }
+        return true;
+    }
+
+    render() {
+        const timeValues = [432, 9, 37, 39];
+        const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
+        let HTML = '';
+
+        for (let i = 0; i < timeValues.length; i++) {
+            HTML += `<div class="time">
+                    <div class="value">${timeValues[i]}</div>
+                    <div class="label">${labelValues[i]}</div>
+                </div>`;
+
+        }
+
+        this.DOM.innerHTML = HTML
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function Clock(selector) {
     const DOM = document.querySelector(selector);
     const timeValues = [432, 9, 37, 39];
     const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
@@ -17,6 +80,6 @@ function Clock(selector) {
     DOM.innerHTML = HTML;
 
 
-}
+} */
 
 export { Clock }
--- js/home.js
+++ js/home.js
@@ -2,5 +2,5 @@ import { Clock } from "./clock/Clock.js";
 import { socialsData } from "./data/socialsData.js";
 import { socials } from "./socials/socials.js";
 
-Clock('#clock_1');
+new Clock('#clock_1');
 socials('footer .socials', socialsData);
